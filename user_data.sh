467714222463f1d315ca2d4618ec091e3a327dd2
Alex Hodges <ahodges22@users.noreply.github.com>
2020-12-11 23:30:40 -0800

    fix: remove resolvd stuff

--- user_data.sh
+++ user_data.sh
@@ -6,10 +6,6 @@ apt-get -y install figlet jq make
 # Generate system banner
 figlet "${welcome_message}" > /etc/motd
 
-# Setup DNS Search domains
-echo 'search ${search_domains}' > '/etc/resolvconf/resolv.conf.d/base'
-resolvconf -u
-
 # Setup local vanity hostname
 echo '${hostname}' | sed 's/\.$//' > /etc/hostname
 hostname `cat /etc/hostname`
