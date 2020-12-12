830db4cfbb2e7f81c077df3db95d06246bdbedf2
Alex Hodges <ahodges22@users.noreply.github.com>
2020-12-11 22:54:58 -0800

    fix: unpin versions

--- versions.tf
+++ versions.tf
@@ -1,19 +1,4 @@
 
 terraform {
   required_version = ">= 0.14.2"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 2.55"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 2.1"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.1"
-    }
-  }
 }
